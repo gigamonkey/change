a91a79eba536508ef41277be8768f90d6f96f4f9
Peter Seibel <peter@gigamonkeys.com>
2016-01-17 21:57:21 -0800

    Simplifying mways and adding benchmarking.

--- change.hs
+++ change.hs
@@ -1,4 +1,10 @@
+import Control.DeepSeq
+import Control.Exception
 import Control.Monad.State
+import Criterion.Main
+import Debug.Trace
+import System.CPUTime
+import Text.Printf
 import qualified Data.Map.Strict as M
 
 us :: Num a => [a]
@@ -14,14 +20,51 @@ us = [1,5,10,25,50]
 -- the ways to compute change for a dollar using the standard us coins
 -- up to half-dollar but for larger amounts it quickly bogs down.
 
-ways [] _ = 0
-ways _ 0  = 1
+ways [] _     = 0
+ways _ 0      = 1
 ways (c:cs) n = if n < 0 then 0 else ways (c:cs) (n - c) + ways cs n
+
+-- The reason this naive approach gets so slow is that the same
+-- problems get computed over and over. In fact to compute the number
+-- of ways to make change for $1.00 with the US coins, ways will be
+-- called 38,901 times with a 165 combinations being called 100 times
+-- or more. And it grows fast: to compute $1.10 it takes 53,301
+-- invocations.
+
+tWays :: [Int] -> Int -> Integer
+tWays [] n     = trace (show (([] :: [Int]), n)) 0
+tWays cs 0     = trace (show (cs, 0)) 1
+tWays (c:cs) n = trace (show ((c:cs), n)) (if n < 0 then 0 else tWays (c:cs) (n - c) + tWays cs n)
+
+fib = (map f [0 .. ] !!) where
+    f 0 = 0
+    f 1 = 1
+    f n = fib (n - 2) + fib (n - 1)
+
+fib' n = (fibs !! n) where
+    fibs = [ f x | x <- [0 .. ] ]
+    f 0 = 0
+    f 1 = 1
+    f n = fibs !! (n - 2) + fibs !! (n - 1)
+
+solutions' = [ ((c, n), ways c n) | n <- [0 .. ], c <- [1 .. length us] ] where
+    ways 0 _ = 0
+    ways _ 0 = 1
+    ways c n | n < 0 = 0
+             | otherwise = ways c (n - (us !! (c - 1))) + ways (c - 1) n
+
+mways coins n = solutions !! idx n (length coins) where
+    solutions = [ twoWays c n | n <- [0 .. ], c <- [1 .. length coins] ]
+    twoWays c n = ways c (n - (coins !! (c - 1))) + ways (c - 1) n
+    ways 0 _ = 0
+    ways _ 0 = 1
+    ways c n = if n < 0 then 0 else solutions !! idx n c
+    idx n c = (n * length coins) + (c - 1)
 
 -- Memoizing version. On the way down we build up a map of every
 -- value we compute so we don't compute them over and over again.
 
-emptyTable :: M.Map ([Int], Int) Int
+emptyTable :: M.Map ([Int], Int) Integer
 emptyTable = M.empty
 
 ways' coins n = fst $ f coins n emptyTable where
@@ -34,26 +77,6 @@ ways' coins n = fst $ f coins n emptyTable where
         compute  = let (a, m') = f cs n m in (a, M.insert (cs, n) a m')
         wrap a   = (a, m)
 
-mfib = (map fib [0..] !!) where
-    fib 0 = 0
-    fib 1 = 1
-    fib n = mfib (n - 2) + mfib (n - 1)
-
-solutions' = [ ways c n | n <- [0..], c <- [0 .. length us] ] where
-    ways 0 _ = 0
-    ways _ 0  = 1
-    ways c n = if n < 0 then 0 else ways c (n - (us !! (c - 1))) + ways (c - 1) n
-
-mways coins n = solutions !! idx (length coins) n where
-    solutions = [ ways c n | n <- [0 .. ], c <- [0 .. length coins] ]
-    ways c n | c == 0 = 0
-             | n == 0 = 1
-             | n < 0 = 0
-             | otherwise = recur c (n - (coins !! (c - 1))) + recur (c - 1) n
-    recur c n | n < 0 = 0
-              | c < 0 = 0
-              | otherwise = solutions !! idx c n
-    idx c n = ((n * (length coins + 1)) + (c - 1)) + 1
 
 -- Memoizing using the State monad. Similar to above except that the
 -- Map of memoized values is threaded through the monad rather than
@@ -66,6 +89,7 @@ ways'' coins n = evalState (f coins n) emptyTable where
     recur c n = get >>= maybe (compute c n) return . M.lookup (c, n)
     compute c n = mapState (memoize (c, n)) $ f c n
     memoize k (x, m) = (x, M.insert k x m)
+
 
 -- Problem with that style of memoizing is that the table we build up
 -- of previously computed values eventually contains every value we've
@@ -110,10 +134,31 @@ halfDollars' = coin quarters' 50
 
 ways''' coins = ((1 : foldl coin (repeat 0) coins) !!)
 
-main = do
-  print $ ways us 100
-  print $ ways' us 100000
-  print $ ways'' us 100000
-  print $ (1:halfDollars) !! 100000
-  print $ (1:halfDollars') !! 100000
-  print $ ways''' us 1000000
+benchmarks = [ ("naive", ways)
+             , ("lazy one", mways)
+             , ("memoizing", ways')
+             , ("memoizing w/State", ways'')
+             , ("lazy two", ways''')
+             ]
+
+time :: ([Int] -> Int -> Integer) -> Int -> IO (Double, Integer)
+time fn n = do
+  start <- getCPUTime
+  x     <- evaluate $ force $ fn us n
+  end   <- getCPUTime
+  let diff = (fromIntegral (end - start)) / (10^12)
+  return (diff, x)
+
+emit :: String -> Int -> (Double, Integer) -> IO ()
+emit label n (time, result) = printf "%s %d: %0.9f sec. Result %d\n" label n time result
+
+check label fn n = do
+  (time, r) <- time fn n
+  emit label n (time, r)
+  if time < 2 then check label fn (n * 10) else return ()
+
+--main =  mapM_ (\(label, fn, n) -> (time (fn :: ([Int] -> Int -> Integer)) n) >>= (emit label n)) $ [ (label, fn, n)  | (label, fn) <- benchmarks, n <- [100, 1000, 10000, 100000, 1000000]]
+
+main = mapM_ (\(label, fn) -> check label fn 1) benchmarks
+
+--main = print $ (tWays us 100)
