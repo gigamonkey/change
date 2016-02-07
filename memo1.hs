6038d66c14a851c702289b24b1c255098be642c0
Peter Seibel <peter@gigamonkeys.com>
2016-02-07 08:36:57 -0800

    Tidying.

--- memo1.hs
+++ memo1.hs
@@ -1,10 +1,18 @@
 module Memo1 (ways) where
 
+-- Similar to fib functions, with a bit of cleverness we can generate
+-- an infinite list of solutions to the change problem for a given
+-- (finite) set of coins by listing for each amount, n, the number of
+-- ways to make change with the first c coins. The recursive call to
+-- the function used to generate this list can index into the list to
+-- get already computed values.
+
 ways :: [Int] -> Int -> Integer
-ways coins amount = solutions !! idx amount (length coins) where
-    solutions = [ twoWays c n | n <- [0 .. ], c <- [1 .. length coins] ]
-    twoWays c n = w c (n - (coins !! (c - 1))) + w (c - 1) n
-    w 0 _ = 0
-    w _ 0 = 1
-    w c n = if n < 0 then 0 else solutions !! idx n c
-    idx n c = (n * length coins) + (c - 1)
+ways coins amount = solutions !! idx amount (length coins)
+    where
+      solutions = [ twoWays c n | n <- [0 .. ], c <- [1 .. length coins] ]
+      twoWays c n = ways' c (n - (coins !! (c - 1))) + ways' (c - 1) n
+      ways' 0 _ = 0
+      ways' _ 0 = 1
+      ways' c n = if n < 0 then 0 else solutions !! idx n c
+      idx n c = (n * length coins) + (c - 1)
--- memo2.hs
+++ memo2.hs
@@ -2,16 +2,19 @@ module Memo2 (ways) where
 
 import qualified Data.Map.Strict as M
 
--- Memoizing version. On the way down we build up a map of every
--- value we compute so we don't compute them over and over again.
+-- Memoizing version. On the way down we build up a map of every value
+-- we compute so we don't compute them over and over again. In this
+-- version we explicitly pass a map along with the list of coins
 
 ways :: [Int] -> Int -> Integer
 ways coins amount = fst $ f coins amount M.empty where
     f [] _ m     = (0, m)
     f _ 0 m      = (1, m)
-    f (c:cs) n m = if n < 0 then (0, m) else (a + a', m'') where
-        (a, m')   = recur (c:cs) (n - c) m
-        (a', m'') = recur cs n m'
-    recur cs n m = maybe compute wrap (M.lookup (cs, n) m) where
-        compute  = let (a, m') = f cs n m in (a, M.insert (cs, n) a m')
-        wrap a   = (a, m)
+    f (c:cs) n m = if n < 0 then (0, m) else (a + a', m'')
+        where
+          (a, m')   = memo (c:cs) (n - c) m
+          (a', m'') = memo cs n m'
+    memo cs n m = maybe compute wrap (M.lookup (cs, n) m)
+        where
+          compute  = let (a, m') = f cs n m in (a, M.insert (cs, n) a m')
+          wrap a   = (a, m)
